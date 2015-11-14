6fd3f79030d0f8726a8a8c8c205c82923dd14076
mikemey <miguel.maestro@gmx.co.uk>
2015-11-14 18:51:48 +0000

    added request logging to file.

--- run.py
+++ run.py
@@ -1,4 +1,13 @@
 from flask import Flask
+from werkzeug import serving
+from werkzeug._internal import _log
+
+
+class LimitLogHandler(serving.WSGIRequestHandler):
+    def log(self, type, message, *args):
+        msg = message % args
+        _log(type, '%s [%s]\n' % (msg, self.client_address[0]))
+
 
 app = Flask(__name__)
 
@@ -13,6 +22,9 @@ def hello():
 if __name__ == "__main__":
     import logging
 
-    logging.basicConfig(filename='/var/log/msm-site.log', level=logging.DEBUG)
+    # logging.basicConfig(filename='msm-site.log', level=logging.DEBUG,
+    #                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    logging.basicConfig(filename='/var/log/msm-site.log', level=logging.DEBUG,
+                        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
-    app.run()
+app.run(request_handler=LimitLogHandler)
