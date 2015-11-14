3d5d3598caad9269ed43b236afafdd8573b6a0f5
mikemey <miguel.maestro@gmx.co.uk>
2015-11-14 11:28:58 -0600

    updated start script to stop current process.

--- start_msm.sh
+++ start_msm.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-# screen -dmS msmsite /usr/bin/python run.py
-python run.py > /var/log/msm-site.log
+H_PID=`ps -ax | grep -e "\smsmsite" | sed 's/\s*\([0-9]*\).*/\1/'`
+if [ -z $H_PID ]; then
+  echo "process not found."
+else
+  echo "shutdown process: [$H_PID]"
+  kill $H_PID >> /dev/null
+fi
+
+screen -dmS msmsite /usr/bin/python run.py
+# python run.py > /var/log/msm-site.log
 
