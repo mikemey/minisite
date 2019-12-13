a2035fd4baec10c6c261aaab370b0d854f6509aa
mikemey <miguel.maestro@gmx.co.uk>
2019-12-13 08:24:07 +0000

    refactor logger, update dependencies

--- run.py
+++ run.py
@@ -1,27 +1,14 @@
-from flask import Flask
+from flask import Flask, request
 from user_agents import parse
-from werkzeug import serving
-from werkzeug._internal import _log
+import logging
 
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(message)s')
+logger = logging.getLogger("main")
 
-class LimitLogHandler(serving.WSGIRequestHandler):
-    def log(self, type, message, *args):
-        host_header = self.headers.getheader('X-Forwarded-For', 'unknown')
-        ua_header = user_agent_from(self.headers.getheader('User-Agent', 'user-agent unknown'))
-        msg = message % args
-        _log(type, '%s [%s - %s]\n' % (msg, host_header, ua_header))
-
-
-def user_agent_from(user_agent_header):
-    parsed = parse(user_agent_header)
-    os = parsed.os.family
-    device = parsed.device.family
-    browser = parsed.browser.family
-    return "%s, %s, %s" % (os, device, browser)
-
+log = logging.getLogger('werkzeug')
+log.disabled = True
 
 app = Flask(__name__)
-
 index = open("index.html").read()
 
 
@@ -30,10 +17,27 @@ def hello():
     return index
 
 
-if __name__ == "__main__":
-    import logging
+@app.after_request
+def after_request(response):
+    remote_addr = request.headers.get('X-Forwarded-For') or request.remote_addr
+    logger.info(
+        "[%s] \"%s %s\" %s (%s)",
+        remote_addr,
+        request.method,
+        request.path,
+        response.status_code,
+        user_agent_from(request.user_agent)
+    )
+    return response
 
-    logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
-app.logger.info('server start...')
-app.run(request_handler=LimitLogHandler, host='127.0.0.1')
+def user_agent_from(user_agent_header):
+    parsed = parse(str(user_agent_header))
+    os = parsed.os.family
+    device = parsed.device.family
+    browser = parsed.browser.family
+    return "%s, %s, %s" % (os, device, browser)
+
+
+logger.info('server start...')
+app.run(host='127.0.0.1', port=5000)
