02a11d0b779b14ea93952ac42406ed5a752be3f3
mikemey <miguel.maestro@gmx.co.uk>
2020-06-09 11:56:06 +0100

    add support for comma-separated ip argument

--- stats.sh
+++ stats.sh
@@ -1,13 +1,16 @@
+#!/usr/bin/env bash
 declare -a all_ips
 
 function extract_ips () {
   for word in $1; do
      if [[ $word =~ [[0-9.]{7,}] ]]; then
-      ip=${word:1:-1}
-      if [[ ! " ${all_ips[@]} " =~ " ${ip} " ]]; then
-         all_ips+=("$ip")
-         printf "\n>> `ip_in_range.py $ip` << "
-      fi
+      IFS=',' read -ra IPS <<< "${word:1:-1}"
+      for ip in "${IPS[@]}"; do
+        if [[ ! " ${all_ips[@]} " =~ " ${ip} " ]]; then
+           all_ips+=("$ip")
+           printf "\n>> `ip_in_range.py $ip` << "
+        fi
+      done
     fi
   done
 }
