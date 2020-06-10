ffd34b4c3a6494fe7873feb87001e958bb8b8d5c
mikemey <miguel.maestro@gmx.co.uk>
2020-06-10 17:02:13 +0200

    add newline after ip-log line

--- stats.sh
+++ stats.sh
@@ -2,17 +2,22 @@
 declare -a all_ips
 
 function extract_ips () {
+  ip_found=false
   for word in $1; do
      if [[ $word =~ [[0-9.]{7,}] ]]; then
       IFS=',' read -ra IPS <<< "${word:1:-1}"
       for ip in "${IPS[@]}"; do
         if [[ ! " ${all_ips[@]} " =~ " ${ip} " ]]; then
+           ip_found=true
            all_ips+=("$ip")
            printf "\n>> `ip_in_range.py $ip` << "
         fi
       done
     fi
   done
+  if $ip_found; then
+    printf "\n"
+  fi
 }
 
 while [[ 1 ]]; do
